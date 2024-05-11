5e8f573e0345ebacbd3fd46dc02300f3095bcd0b
Jian <jianrliu21@gmail.com>
2024-05-11 13:49:53 -0400

    first

---
+++ Spring/JMovies/JMovies/src/test/java/com/JMovies/JMovies/JMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JMovies.JMovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
